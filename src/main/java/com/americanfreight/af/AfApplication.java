c69c4148935383ec77cd224229f6086b56c72bc4
myszon01 <myszon01@gmail.com>
2017-08-01 23:44:11 -0500

    upload

---
+++ src/main/java/com/americanfreight/af/AfApplication.java
@@ -0,0 +1,12 @@
+package com.americanfreight.af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/americanfreight/af/AfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.americanfreight.af;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
